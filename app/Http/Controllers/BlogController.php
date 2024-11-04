3221bace9e065c37e6d97dcbf941faedd2c9628f
KAIZER445 <prashanttamang861@gmail.com>
2024-11-04 17:33:46 +0545

    create part of the blog is complete

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\MyModel;
+use App\Models\TempImage;
+use Illuminate\Support\Facades\File;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -30,7 +32,7 @@ class BlogController extends Controller
                 'description' => 'nullable|string',
                 'shortDes' => 'nullable|string'
             ]);
-    
+
             if ($validator->fails()) {
                 return response()->json([
                     'status' => false,
@@ -38,11 +40,30 @@ class BlogController extends Controller
                     'errors' => $validator->errors()
                 ], 400);
             }
-    
+
             $blog = new MyModel();
             $blog->fill($request->only(['title', 'author', 'description', 'shortDec']));
             $blog->save();
-    
+
+            // save image here
+
+            $tempImage = TempImage::find($request->image_id);
+
+            if($tempImage!=null) {
+
+                $imageExtArray = explode('.',$tempImage->name);
+                $ext = last($imageExtArray);
+                $imageName = time().'-'.$blog->id.'.'.$ext;
+
+                $blog->image = $imageName;
+                $blog->save();
+
+
+                $sourcePath = public_path('uploads/temp/'.$tempImage->name);
+                $destPath = public_path('uploads/blogs/'.$imageName);
+                File::copy($sourcePath, $destPath);
+            }
+
             return response()->json([
                 'status' => true,
                 'message' => 'Data saved successfully',
@@ -55,7 +76,7 @@ class BlogController extends Controller
             ], 500);
         }
     }
-    
+
 
     // This method will update blogs
     public function update()
